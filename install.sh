f043af01eec01dd0afc75832a77f56b0bb114de3
Sergey <megatestbot@gmail.com>
2024-09-23 11:45:46 +0300

    Unattended upgrades are switched off in the install.sh

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+sudo ln -f -s /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+sudo apt-get -y install feh conky unclutter wmctrl exiftran exif exifprobe
+
+sudo systemctl enable chrony
+sudo systemctl stop chrony
+sudo systemctl start chrony
+sudo chronyc makestep
+
+sudo systemctl enable cron
+sudo systemctl stop cron
+sudo systemctl start cron
+
+
+sudo systemctl stop unattended-upgrades
+sudo apt-get purge unattended-upgrades
+sudo chmod -x /etc/update-motd.d/40-orangepi-updates
+sudo sed -i 's/Prompt=.*/Prompt=never/' /etc/update-manager/release-upgrades
+
+sudo systemctl stop apt-daily.timer
+sudo systemctl stop apt-daily-upgrade.timer
+sudo systemctl stop apt-daily.service
+sudo systemctl stop apt-daily-upgrade.service
+sudo systemctl disable apt-daily.timer
+sudo systemctl disable apt-daily-upgrade.timer
+sudo systemctl mask apt-daily.service
+sudo systemctl mask apt-daily-upgrade.service
+sudo systemctl stop update-notifier-download.timer
+sudo systemctl stop update-notifier-download.service
+sudo systemctl disable update-notifier-download.service
+sudo systemctl disable update-notifier-download.timer
+sudo systemctl mask update-notifier-download.service
+
+sudo systemctl daemon-reload
+sudo systemctl reset-failed
+sudo systemctl list-timers --all
+
