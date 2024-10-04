35b5cbeb1b40d5c0a96bce427122df0323c0c3bf
Sergey <megatestbot@gmail.com>
2024-10-04 20:49:30 +0300

    update logic moved from install.sh into separate script

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-set -v
-
-#bash <(curl -s https://raw.githubusercontent.com/Stary/frame/refs/heads/main/install.sh)
 
 if [ "$EUID" -eq 0 ]
   then echo "Please do not run the script as root"
@@ -11,38 +8,6 @@ fi
 
 USER=`whoami`
 SRC_DIR="$(cd $(dirname $(realpath "$0")); pwd -P)"
-HOME_DIR=`eval echo ~$USER`
-MEDIA_DIR="$HOME_DIR/photo"
-DEMO_DIR="$HOME_DIR/demo"
-CONF_DIR="$HOME_DIR/.config/conky"
-BIN_DIR="$HOME_DIR/bin"
-LOG_DIR="/var/log/frame"
-SSH_DIR=$HOME/.ssh
-SSH_KEYS=$SSH_DIR/authorized_keys
-
-CONF="conky.conf"
-FONT="UbuntuThin.ttf"
-MAIN_SCRIPT="frame_watchdog.sh"
-INFO_SCRIPT="get_info.sh"
-LOG_FILE="frame.log"
-
-if [ ! -d "$DEMO_DIR" ]
-then
-  mkdir $DEMO_DIR
-  unzip -d $DEMO_DIR $SRC_DIR/demo.zip
-fi
-
-if [ ! -d "$SSH_DIR" ]
-then
-  mkdir -p $SSH_DIR
-  chmo 700 $SSH_DIR
-fi
-
-if [ ! -s "$SSH_KEYS" ]
-then
-  cat $SRC_DIR/keys.txt >> $SSH_KEYS
-  chmod 600 $SSH_KEYS
-fi
 
 SUDO_READY=`sudo cat /etc/sudoers | grep $USER | grep -E -e "NOPASSWD:\s*ALL" | wc -l`
 
@@ -51,26 +16,10 @@ then
   echo "$USER ALL=(ALL) NOPASSWD: ALL" | sudo EDITOR='tee -a' visudo
 fi
 
-for d in $MEDIA_DIR $DEMO_DIR $CONF_DIR $BIN_DIR $LOG_DIR
-do
-  echo $d
-  sudo mkdir -p $d
-  sudo chown -R $USER $d
-done
-
-if [ $SRC_DIR != $BIN_DIR ];
-then
-  rsync -av $SRC_DIR/$MAIN_SCRIPT $BIN_DIR
-  rsync -av $SRC_DIR/$INFO_SCRIPT $BIN_DIR
-  #remove outdated script
-  rm -f $BIN_DIR/get_date.sh
-fi
-rsync -av $SRC_DIR/$CONF $CONF_DIR
-rsync -av $SRC_DIR/$FONT $CONF_DIR
-
 sudo ln -f -s /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 sudo localectl set-locale C.UTF-8
 
+export DEBIAN_FRONTEND=noninteractive
 APT_OPTIONS="--allow-unauthenticated --allow-downgrades --allow-remove-essential --allow-change-held-packages"
 sudo apt-get --yes update
 sudo apt-get --yes $APT_OPTIONS -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
@@ -124,11 +73,5 @@ sudo sed -i 's/^APT/#APT/' /etc/apt/apt.conf.d/99update-notifier
 
 sudo sed -i 's/Unattended-Upgrade "7"/Unattended-Upgrade "0"/' /etc/apt/apt.conf.d/02-orangepi-periodic
 
-(crontab -l 2>/dev/null| grep -v $MAIN_SCRIPT; echo "* * * * * $BIN_DIR/$MAIN_SCRIPT >> $LOG_DIR/$LOG_FILE 2>&1") | crontab -
-
-pkill conky
-pkill unclutter
-pkill feh
-
-$BIN_DIR/$MAIN_SCRIPT
+$SRC_DIR/update.sh
 
---
+++ update.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+set -v
+
+#bash <(curl -s https://raw.githubusercontent.com/Stary/frame/refs/heads/main/install.sh)
+
+if [ "$EUID" -eq 0 ]
+  then echo "Please do not run the script as root"
+  exit
+fi
+
+USER=`whoami`
+SRC_DIR="$(cd $(dirname $(realpath "$0")); pwd -P)"
+HOME_DIR=`eval echo ~$USER`
+MEDIA_DIR="$HOME_DIR/photo"
+DEMO_DIR="$HOME_DIR/demo"
+CONF_DIR="$HOME_DIR/.config/conky"
+BIN_DIR="$HOME_DIR/bin"
+LOG_DIR="/var/log/frame"
+SSH_DIR=$HOME/.ssh
+SSH_KEYS=$SSH_DIR/authorized_keys
+
+CONF="conky.conf"
+FONT="UbuntuThin.ttf"
+MAIN_SCRIPT="frame_watchdog.sh"
+INFO_SCRIPT="get_info.sh"
+LOG_FILE="frame.log"
+
+if [ ! -d "$SSH_DIR" ]
+then
+  mkdir -p $SSH_DIR
+  chmo 700 $SSH_DIR
+fi
+
+if [ ! -s "$SSH_KEYS" ]
+then
+  cat $SRC_DIR/keys.txt >> $SSH_KEYS
+  chmod 600 $SSH_KEYS
+fi
+
+if [ ! -d "$DEMO_DIR" ]
+then
+  mkdir $DEMO_DIR
+  unzip -d $DEMO_DIR $SRC_DIR/demo.zip
+fi
+
+for d in $MEDIA_DIR $DEMO_DIR $CONF_DIR $BIN_DIR $LOG_DIR
+do
+  echo $d
+  sudo mkdir -p $d
+  sudo chown -R $USER $d
+done
+
+if [ $SRC_DIR != $BIN_DIR ];
+then
+  rsync -av $SRC_DIR/$MAIN_SCRIPT $BIN_DIR
+  rsync -av $SRC_DIR/$INFO_SCRIPT $BIN_DIR
+  #remove outdated script
+  rm -f $BIN_DIR/get_date.sh
+fi
+rsync -av $SRC_DIR/$CONF $CONF_DIR
+rsync -av $SRC_DIR/$FONT $CONF_DIR
+
+
+(crontab -l 2>/dev/null| grep -v $MAIN_SCRIPT; echo "* * * * * $BIN_DIR/$MAIN_SCRIPT >> $LOG_DIR/$LOG_FILE 2>&1") | crontab -
+
+pkill conky
+pkill unclutter
+pkill feh
+
+$BIN_DIR/$MAIN_SCRIPT
+
