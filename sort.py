b624045eed6a677639c152197f8d51517cc9df23
sergey <megatestbot@gmail.com>
2024-10-06 17:35:57 +0300

    force_exif option has been added to ignore date set in filename and extract it from the EXIF tags

--- sort.py
+++ sort.py
@@ -4,29 +4,29 @@ import re
 import platform
 import time
 import subprocess
-import uuid
 
-def creation_date(path_to_file):
+def creation_date(path_to_file, force_exif=False):
 
     filename = os.path.basename(path_to_file)
-    #print(f"{path_to_file=} {filename=}")
-    if re.match(r'^20[0-9]{6}\_[0-9]{6}', filename):
+    print(f"{path_to_file=} {filename=}")
+    if not force_exif and re.match(r'^20[0-9]{6}\_[0-9]{6}', filename):
         time_obj = time.strptime(filename[0:15], '%Y%m%d_%H%M%S')
         print(f"Timestamp extracted from filename {filename}: {time_obj}")
         return time.mktime(time_obj)
 
     try:
-        output=subprocess.run(['exiftool', path_to_file], stdout=subprocess.PIPE).stdout.decode('utf-8')
+        output=subprocess.run(['exiftool', '-n', path_to_file], stdout=subprocess.PIPE).stdout.decode('utf-8')
         exif = dict()
         for line in output.splitlines():
             attr = re.split(r'\s*:\s+', line, maxsplit=1)
             if len(attr) == 2:
                 exif[attr[0]] = attr[1]
-        for p in ('Create Date', 'Date Created', 'Date/Time Original'):
+                print(f"{attr[0]} = {attr[1]}")
+        for p in ('Create Date', 'Date Created', 'Date/Time Original', 'GPS Date/Time'):
             if p in exif:
                 try:
                     time_obj = time.strptime(exif[p][0:19], '%Y:%m:%d %H:%M:%S')
-                    #print(f"EXIF: {p} = {exif[p]} = {time_obj}")
+                    print(f"EXIF: {p} = {exif[p]} = {time_obj}")
                     if int(time_obj.tm_year) >= 1990:
                         return time.mktime(time_obj)
                 except ValueError as ve:
@@ -54,7 +54,7 @@ def creation_date(path_to_file):
 #exit(0)
 
 source_dir=''
-dirs = ["/Users/sergey/Photo/icloud", "/home/orangepi/frame"]
+dirs = ["/Users/sergey/Photo/icloud_test", "/home/orangepi/frame"]
 for d in dirs:
     if os.path.isdir(d):
         source_dir = d
@@ -78,7 +78,7 @@ for i in sorted(os.scandir(source_dir), key=lambda e: e.name):
         #name2 = re.sub(r'[\W_ийеё]+', '', name.lower())
 
         full_path = i.path
-        ctime = time.localtime(creation_date(full_path))
+        ctime = time.localtime(creation_date(full_path, force_exif=True))
         #print(f"{full_path} {ctime=}")
         #name3 = f"{ctime.tm_year}{ctime.tm_mon:02d}{ctime.tm_mday:02d}_{ctime.tm_hour:02d}{ctime.tm_min:02d}{ctime.tm_sec:02d}"
         ct = f"{ctime.tm_year}{ctime.tm_mon:02d}{ctime.tm_mday:02d}_{ctime.tm_hour:02d}{ctime.tm_min:02d}{ctime.tm_sec:02d}"
