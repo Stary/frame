c83c2b6ccf66587bc1f16663b35ceff45630b8b9
Sergey <megatestbot@gmail.com>
2024-10-07 21:22:31 +0300

    day of week and time of the shot are added into info

--- get_info.sh
+++ get_info.sh
@@ -6,14 +6,23 @@ ts=''
 
 if [ -s "$f" ]
 then
-  ts=`exiftool $1 2>/dev/null | grep -i date | grep -i -E -e "(create|gps|original)" | sort | head -1 | sed -E "s/^[^\:]+\:\s*([0-9]+)[^0-9]*([0-9]+)[^0-9]*([0-9]+).*/\1.\2.\3/"`
+  ts=`exiftool $1 2>/dev/null | grep -i date | grep -i -E -e "(create|gps|original)" | sort | head -1 | sed -E "s/^[^\:]+\:\s*([0-9]+)[^0-9]*([0-9]+)[^0-9]*([0-9]+)[^0-9]*([0-9]+)[^0-9]*([0-9]+).*/\1.\2.\3 \4:\5/"`
 fi
+
+ts=''
 
 if [ -z "$ts" ]
 then
-    ts=`echo $1 | sed -E "s/^.*+\///g" | sed -E "s/\.[^\.]+$//" | sed -E "s/^.*?([0-9]{4})([0-9]{2})([0-9]{2})\_[0-9]+.*$/\1.\2.\3/g"`
+    ts=`echo $1 | sed -E "s/^.*+\///g" | sed -E "s/\.[^\.]+$//" | sed -E "s/^.*?([0-9]{4})([0-9]{2})([0-9]{2})\_([0-9]{2})([0-9]{2})[0-9]+.*$/\1.\2.\3 \4:\5/g"`
 fi
 
-echo $ts
-~/bin/get_place.py $f
+
+if [ ! -z "$ts" ]
+then
+  dow=`date --date $(sed "s/\./-/g" <<< "2017.01.01") +%a`
+fi
+
+place=$(~/bin/get_place.py $f)
+
+echo $dow $ts $place
 
