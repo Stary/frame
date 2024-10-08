772c1a2379112a3c7d5f0d0ad5db22b9a811f96e
Sergey <megatestbot@gmail.com>
2024-10-08 22:56:34 +0300

    Date localized

--- get_info.sh
+++ get_info.sh
@@ -6,23 +6,26 @@ ts=''
 
 if [ -s "$f" ]
 then
-  ts=`exiftool $1 2>/dev/null | grep -i date | grep -i -E -e "(create|gps|original)" | sort | head -1 | sed -E "s/^[^\:]+\:\s*([0-9]+)[^0-9]*([0-9]+)[^0-9]*([0-9]+)[^0-9]*([0-9]+)[^0-9]*([0-9]+).*/\1.\2.\3 \4:\5/"`
+  ts=`exiftool $1 2>/dev/null | grep -i date | grep -i -E -e "(create|gps|original)" | sort | head -1 | sed -E "s/^[^\:]+\:\s*([0-9]+)[^0-9]*([0-9]+)[^0-9]*([0-9]+)[^0-9]*([0-9]+)[^0-9]*([0-9]+)[^0-9]*([0-9]+).*/\1-\2-\3 \4:\5:\6/"`
 fi
-
-ts=''
 
 if [ -z "$ts" ]
 then
-    ts=`echo $1 | sed -E "s/^.*+\///g" | sed -E "s/\.[^\.]+$//" | sed -E "s/^.*?([0-9]{4})([0-9]{2})([0-9]{2})\_([0-9]{2})([0-9]{2})[0-9]+.*$/\1.\2.\3 \4:\5/g"`
+  ts=`echo $1 | sed -E "s/^.*+\///g" | sed -E "s/\.[^\.]+$//" | sed -E "s/^.*?([0-9]{4})([0-9]{2})([0-9]{2})\_([0-9]{2})([0-9]{2})([0-9]{2}).*$/\1-\2-\3 \4:\5:\6/g"`
 fi
 
 
 if [ ! -z "$ts" ]
 then
-  dow=`date --date $(sed "s/\./-/g" <<< "$ts" | sed "s/ .*//") +%a`
+  export LC_ALL=ru_RU.UTF-8
+  ts2=`date --date "$ts"`
+  if [ ! -z "$ts2" ]
+  then
+    ts=$ts2
+  fi
 fi
 
 place=$(~/bin/get_place.py $f)
 
-echo $dow $ts $place
+echo $ts $place
 
