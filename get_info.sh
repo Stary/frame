473b6ba43647118baf0ce16f300924c8eb85f32a
sergey <megatestbot@gmail.com>
2024-10-10 22:19:40 +0300

    filenames containing spaces are supported

--- get_info.sh
+++ get_info.sh
@@ -6,12 +6,12 @@ ts=''
 
 if [ -s "$f" ]
 then
-  ts=`exiftool $1 2>/dev/null | grep -i date | grep -i -E -e "(create|gps|original)" | sort | head -1 | sed -E "s/^[^\:]+\:\s*([0-9]+)[^0-9]*([0-9]+)[^0-9]*([0-9]+)[^0-9]*([0-9]+)[^0-9]*([0-9]+)[^0-9]*([0-9]+).*/\1-\2-\3 \4:\5:\6/"`
+  ts=`exiftool "$1" 2>/dev/null | grep -i date | grep -i -E -e "(create|gps|original)" | sort | head -1 | sed -E "s/^[^\:]+\:\s*([0-9]+)[^0-9]*([0-9]+)[^0-9]*([0-9]+)[^0-9]*([0-9]+)[^0-9]*([0-9]+)[^0-9]*([0-9]+).*/\1-\2-\3 \4:\5:\6/"`
 fi
 
 if [ -z "$ts" ]
 then
-  ts=`echo $1 | sed -E "s/^.*+\///g" | sed -E "s/\.[^\.]+$//" | sed -E "s/^.*?([0-9]{4})([0-9]{2})([0-9]{2})\_([0-9]{2})([0-9]{2})([0-9]{2}).*$/\1-\2-\3 \4:\5:\6/g"`
+  ts=`echo $1 | sed -E "s/^.*\///g" | sed -E "s/\.[^\.]+$//" | grep -E -e "^[0-9_]+$" | sed -E "s/^.*?([0-9]{4})([0-9]{2})([0-9]{2})\_([0-9]{2})([0-9]{2})([0-9]{2}).*$/\1-\2-\3 \4:\5:\6/g"`
 fi
 
 
@@ -25,11 +25,10 @@ then
   fi
 fi
 
-place=$(~/bin/get_place.py $f)
+place=$(~/bin/get_place.py "$f")
 
-echo $ts 
+echo $ts
 if [ ! -z "$place" ]
 then
   echo $place
 fi
-
