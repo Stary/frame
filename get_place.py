64d5383cb09cbb2d7833d745ec94e729fef9c3cd
sergey <megatestbot@gmail.com>
2024-10-14 20:22:24 +0300

    exceptions handling while getting EXIF data

--- get_place.py
+++ get_place.py
@@ -22,20 +22,23 @@ if os.path.isfile(path_to_file):
             #print(f"{m.group(0)}|{m.group(1)}|{m.group(2)}|{m.group(3)}|{m.group(4)}")
             place_descr = m.group(1)
             place_radius = float(m.group(2)) * (0.001 if m.group(3).lower() == 'm' else 1)
-        output=subprocess.run(['exiftool', '-n', path_to_file], stdout=subprocess.PIPE).stdout.decode('utf-8')
+        output=subprocess.run(['exiftool', '-n', path_to_file], stdout=subprocess.PIPE).stdout #.decode('utf-8')
         exif = dict()
         for line in output.splitlines():
             attr = re.split(r'\s*:\s+', line, maxsplit=1)
             if len(attr) == 2:
                 exif[attr[0]] = attr[1]
         if 'GPS Latitude' in exif and 'GPS Longitude' in exif:
-            latitude = float(exif['GPS Latitude'])
-            longitude = float(exif['GPS Longitude'])
-            if place_descr is not None and place_descr != '':
-                geo.set_place_descr(latitude, longitude, place_descr, place_radius)
-            else:
-                place_descr = geo.get_place_descr(latitude, longitude)
-            print(f"{place_descr}")
+            try:
+                latitude = float(exif['GPS Latitude'])
+                longitude = float(exif['GPS Longitude'])
+                if place_descr is not None and place_descr != '':
+                    geo.set_place_descr(latitude, longitude, place_descr, place_radius)
+                else:
+                    place_descr = geo.get_place_descr(latitude, longitude)
+                print(f"{place_descr}")
+            except ValueError:
+                pass
     except Exception as e:
         print(f"Exception occured while processing file {path_to_file}: {repr(e)}")
 
