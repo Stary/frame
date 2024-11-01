4e09079e2ad56f44a06dfbcc90eb66e06aeb7193
sergey <megatestbot@gmail.com>
2024-11-01 10:56:46 +0300

    Новый скрипт для получения номера версии по истории коммитов

---
+++ get_version.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+history=$(git log -n 1000 --format=raw | grep author | sed 's/.*>\s*//' | sed 's/\s.*//' | xargs -I '{}' date -d @{} +'%Y.%m.%d')
+version='0.0.0'
+
+if [ -n "$history" ]
+then
+  last_date=$(echo "$history" | head -1)
+  if [ -n "$last_date" ]
+  then
+    count=$(echo "$history" | grep $last_date | wc -l)
+    if [ "$count" -gt "1" ]
+    then
+      version="$last_date-$count"
+    else
+      version="$last_date"
+    fi
+  fi
+fi
+
+echo $version
+ 
+
