b84932d37e22bef08d3b2f91f809a5077e9fa34a
sergey <megatestbot@gmail.com>
2024-12-03 21:55:27 +0300

    Информационное сообщение адаптируется в зависимости от наличия флэшки и установленного подключения к сети

--- get_info.sh
+++ get_info.sh
@@ -7,23 +7,37 @@ max_len=$2
 
 ts=''
 
-export LC_ALL=ru_RU.UTF-8
-export LANG=ru_RU.UTF-8
-
 BIN_DIR=$HOME/bin
+USB_DIR=/media/usb
 MEDIA_USER=media
 MEDIA_PASSWD=$(cat ~/user.dat)
 IP=$(ifconfig | grep inet | grep -v inet6 | grep -v 127.0.0.1 | sed 's/.*inet *//' | sed 's/ *netmask.*//')
 WIFI_SSID=$(sudo nmcli --fields CONNECTION,STATE,DEVICE  d | grep -v -i disconnected | grep -i connected | grep wlan | cut -d ' ' -f 1)
+USB_READY=0
+if [ $(mount | grep -c $USB_DIR) -gt "0" ] && [ -w "$USB_DIR" ]
+then
+  USB_READY=1
+fi
 
 uptime=$(awk '{print $1}' /proc/uptime | sed 's/\..*//')
 changed_files=$(find $BIN_DIR -type f -mmin -5 | grep -v pycache)
+
 if [ "$uptime" -lt "300" ] || [ -n "$changed_files" ]
 then
   echo "Фоторамка v.$VERSION"
   echo ""
-  echo "Просто вставьте флэшку с фотографиями и наслаждайтесь воспоминаниями!"
-  echo ""
+  if [ "X$USB_READY" != "X1" ]
+  then
+    echo "Просто вставьте флэшку с фотографиями и наслаждайтесь воспоминаниями!"
+    echo ""
+    echo "Для стабильной работы флэшку необходимо предварительно отформатировать в FAT32"
+    echo "на компьютере под управлением операционной системы Windows 10/11."
+    echo ""
+    echo "Настройки слайдшоу и часов будут сохранены на флэшке в файле frame.cfg,"
+    echo "их можно редактировать как по сети, так и подключив временно флэшку к компьютеру."
+    echo ""
+  fi
+
   if [ -z "$IP" ] || [ -z "$WIFI_SSID" ]
   then
     echo "Для подключения рамки к сети создайте на флэшке файл wifi.txt с двумя строками:"
@@ -36,16 +50,17 @@ then
     echo "Порт: 22"
     echo "Пользователь: $MEDIA_USER"
     echo "Пароль: $MEDIA_PASSWD"
-    echo "Папка с фотографиями на флэшке: /media/usb"
     echo "Папка для фотографий на MicroSD: /media/photo"
+    if [ "X$USB_READY" == "X1" ]
+    then
+      echo "Папка с фотографиями на флэшке: /media/usb"
+    fi
   fi
-  echo ""
-  echo "Для стабильной работы флэшку необходимо предварительно отформатировать в FAT32"
-  echo "на компьютере под управлением операционной системы Windows 10/11."
-  echo "Настройки слайдшоу и часов будут сохранены на флэшке в файле frame.cfg,"
-  echo "их можно редактировать как по сети, так и подключив временно флэшку к компьютеру."
   exit
 fi
+
+export LC_ALL=ru_RU.UTF-8
+export LANG=ru_RU.UTF-8
 
 if [ -s "$f" ]
 then
