af5a8888c19b7372a5c1a8ec80a4d77969ede24d
Sergey Alekseev <sergey@alarislabs.com>
2026-01-11 01:36:21 +0300

    Отладка скрипт подготовки образа

--- prepare_frame_image.sh
+++ prepare_frame_image.sh
@@ -158,7 +158,7 @@ if [ -n "$IMAGE_SIZE" ]; then
         exit 1
     fi
 
-    parted -s "$DISK_DEVICE" unit s resizepart "$PARTITION_NUM" "${END_SECTOR}s" yes
+    yes | parted -s "$DISK_DEVICE" unit s resizepart "$PARTITION_NUM" "${END_SECTOR}s"
     parted "$DISK_DEVICE" print
 
     partprobe "$DISK_DEVICE" 2>/dev/null || blockdev --rereadpt "$DISK_DEVICE"
